094e3386e69620f285ba8d543e80f5f38bf7e63f
Josh Sauer <joshsauer@yahoo.com>
2021-02-22 00:33:23 +0000

    Trying to fix

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["\n1) Who was the first American woman in space? ", "\n2) True or false: 5000 meters = 5 kilometers. ", "\n3) (5+3)/2*10 =? ", "\n4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "\n5) What is the mininum crew size for the ISS? "];
+let questions = ["\n1) Who was the first American woman in space? ", "\n2) True or false: 5000 meters == 5 kilometers. ", "\n3) (5+3)/2*10 = ? ", "\n4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "\n5) What is the mininum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
